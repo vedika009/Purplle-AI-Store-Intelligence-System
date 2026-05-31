d7fbf9a548827136941309b0dea173dc931245f3
vedika009 <vedikamishra009@gmail.com>
2026-05-31 23:52:22 +0530

    chore: remove scratch files

--- scratch/get_dims.py
+++
@@ -1,5 +0,0 @@
-import cv2
-cap = cv2.VideoCapture('./data/raw/CCTV Footage-20260529T160731Z-3-00144614ea/CCTV Footage/CAM 1.mp4')
-print("Width:", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-print("Height:", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-cap.release()
